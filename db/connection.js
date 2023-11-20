2f0fc7ddc7306659d886828001a26404992e936d
Dominic20227 <dominicmalloy2@outlook.com>
2023-11-20 10:50:00 +0000

    completed task 1

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,16 @@
-const { Pool } = require('pg');
-const ENV = process.env.NODE_ENV || 'development';
+const { Pool } = require("pg");
+const ENV = process.env.NODE_ENV || "development";
+console.log(ENV);
 
-require('dotenv').config({
-  path: `${__dirname}/../.env.${ENV}`,
+// const pathToEnvFile = "../.env.devlopment";
+const pathToEnvFile = `${__dirname}/../.env.${ENV}`;
+
+require("dotenv").config({
+  path: pathToEnvFile,
 });
 
 if (!process.env.PGDATABASE) {
-  throw new Error('PGDATABASE not set');
+  throw new Error("PGDATABASE not set");
 }
 
 module.exports = new Pool();
